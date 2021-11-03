a8c0f23e869f8a7cda454497e8c70e26d24d1b21
idc04158 <idc04158@naver.com>
2021-11-03 17:43:14 +0900

    php connect db

---
+++ DB_testing.php
@@ -0,0 +1,2 @@
+<?php $connect_no = mysqli_connect("localhost", "root", "", "temp_db"); //return vaule
+echo("접속이 되었다는 증거" . $connect_no); ?>
--- member_register.php
+++ member_register.php
@@ -2,11 +2,44 @@
 <html>
 <meta charset="UTF-8">
 <head>
+    <meta charset="UTF-8">
+    <title>member_register</title>
+    <link rel="stylesheet" href="member_register.css">
 </head>
 <body>
-<?php
-//$_GET["u_name"];
-//$u_name = $_post["u_name"];
-echo("이 사람은 " . $_POST["u_name"]);
-?>
+<table border =2>
+    <tr>
+        <td class="table_name">
+            회원가입정보</td>
+    </tr>
+    <tr>
+        <td>이름</td>
+        <td><?=($_POST["u_name"])?></td>
+    </tr>
+    <tr>
+        <td>
+            주소</td>
+        <td bgcolor = gray>
+            <?php echo($_POST["u_addr"])?></td>
+    </tr>
+    <tr>
+        <td>
+            핸드폰번호</td>
+        <td>
+            <?php echo($_POST["u_phone"])?></td>
+    </tr>
+    <tr>
+        <td>국외여행여부</td>
+        <td><?=($_POST["u_tour"])?></td>
+    </tr>
+    <tr>
+        <td>이메일주소</td>
+        <td><?=($_POST["u_email"])?></td>
+    </tr>
+    <tr>
+        <td>주민등록번호 뒷자리</td>
+        <td><?=($_POST["u_personal_id"])?></td>
+    </tr>
+    
+</table>
 </body>
