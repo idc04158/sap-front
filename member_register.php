8a906bbdba19f40087b0dcd9b9a25622bdcc715c
idc04158 <idc04158@naver.com>
2021-11-03 14:39:33 +0900

    first

---
+++ member_register.php
@@ -0,0 +1,42 @@
+<?php
+    echo("안녕");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>member_register</title>
+    <link rel="stylesheet" href="member_register.css">
+</head>
+<body>
+   
+    <div class=background>
+        <h1>SIGN IN PAGE</h1>
+        <form class="input" action="https://temp.php">
+            <input type="text" value="회원이름">
+            <input type="number" value="핸드폰 번호">
+            <input type="text" value="주소">
+            <input type="text" value="이메일 주소">
+            <input type="password" value="주민등록번호 뒷자리">
+            <p>14일 이내 국외여행 여부</p>
+            <p>예<input type="radio"></p>
+            <p>아니요<input type="radio"></p>
+            <p>성별</p>
+            남<input type="radio"><br>
+            여<input type="radio"><br>
+            기입을 희망하지 않음<input type="radio"><br>
+            <div class="btn>"><input type="submit" value="회원가입"></div>
+        </form>
+        <footer>
+            <div>
+                어쩌구회사<br>
+                TEL: 010-7797-6892<br>
+                사업자 등록번호: 000-000-0000<br>
+            </div>
+        </footer>
+        <div>
+            
+        </div>
+    </div>
+</body>
+</html>
