1038b67e891111a8f23f127528f28e110b79f5eb
idc04158 <idc04158@naver.com>
2021-11-04 18:02:12 +0900

    homework

--- login.php
+++ login.php
@@ -6,12 +6,18 @@
 
  $id=mysqli_connect("localhost", "root", "","login_db" );//접속 
 
- $result = mysqli_query($id, "SELECT * FROM login_t 
-	WHERE user_name LIKE '%$search_id%'");
+$result = mysqli_query($id, "SELECT * FROM login_t 
+	WHERE user_id LIKE '%$search_id%'");
+$result = mysqli_query($id, "SELECT * FROM login_t 
+    WHERE user_pw LIKE '%$search_pw%'");
+
+// if(일치하면){
+//     echo("회원입니다")
+// }else{echo"회원이 아닙니다"}
 
  $record = mysqli_fetch_array( $result );
- echo "핸폰번호: " . $record['u_phone'];
- echo "주소: " . $record['u_addr'] . "<hr>";
+ echo "이름: " . $record['user_name'];
+ echo "식별번호" . $record['u_id'] . "<hr>";
 
  mysqli_close($id); //접속종료
 ?>	
